d5b9a9059dce392a85045ee9d0d19ceb67456c08
tfullington <tswmcello@me.com>
2017-01-21 21:43:44 -0500

    Update LED_Control.py

--- LED_Control.py
+++ LED_Control.py
@@ -13,14 +13,14 @@ while(1):
     #Ramps brightness from 0 to 200 and back over 1 second + time to execute
     brightness = 0
     for n in range(10):
-        brightness =+ 10 
+        brightness =+ 20 
         pi1.set_PWM_dutycycle(13, brightness)
         pi1.set_PWM_dutycycle(26, brightness)
         pi1.set_PWM_dutycycle(19, brightness)
         time.sleep(0.05)
         
     for n in range(10):
-        brightness =- 10 
+        brightness =- 20 
         pi1.set_PWM_dutycycle(13, brightness)
         pi1.set_PWM_dutycycle(26, brightness)
         pi1.set_PWM_dutycycle(19, brightness)

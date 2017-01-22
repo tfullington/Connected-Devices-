69ed199b973fba4352df4b1d9e3a89c845d52f29
tfullington <tswmcello@me.com>
2017-01-21 21:32:21 -0500

    Update LED_Control.py

--- LED_Control.py
+++ LED_Control.py
@@ -1,13 +1,27 @@
 import time
+import pigpio
 pi1 = pigpio.pi()
 
 while(1):
     #Initializes all LED's to off
-    pi1.write(19,1)
-    pi1.write(26,1)
-    pi1.write(13,1)
+    pi1.write(19,0)
+    pi1.write(26,0)
+    pi1.write(13,0)
     
     time.sleep(1)
-    pi1.set_PWM_dutycycle(19, 128)
-    pi1.set_PWM_dutycycle(26, 128)
-    pi1.set_PWM_dutycycle(13, 128)
+    
+    #Ramps brightness from 0 to 200 and back over 1 second + time to execute
+    brightness = 0
+    for n in range(10):
+        brightness =+ 10 
+        pi1.set_PWM_dutycycle(13, brightness)
+        pi1.set_PWM_dutycycle(26, brightness)
+        pi1.set_PWM_dutycycle(19, brightness)
+        time.sleep(0.05)
+        
+    for n in range(10):
+        brightness =- 10 
+        pi1.set_PWM_dutycycle(13, brightness)
+        pi1.set_PWM_dutycycle(26, brightness)
+        pi1.set_PWM_dutycycle(19, brightness)
+        time.sleep(0.05)

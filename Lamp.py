8c5496caafebc6013e881fafcbb437aa876f7347
tfullington <tswmcello@me.com>
2017-01-27 01:01:03 -0500

    Update Lamp.py

--- Lamp.py
+++ Lamp.py
@@ -1,7 +1,7 @@
 import pigpio
 
 pi1 = pigpio.pi()
-brightness = 300
+brightness = 255
 pi1.set_PWM_dutycycle(13, brightness)
 pi1.set_PWM_dutycycle(26, brightness)
 pi1.set_PWM_dutycycle(19, brightness)

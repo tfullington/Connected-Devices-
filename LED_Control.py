78b5bc682109af8617ea9ed68375d0b244458d7c
tfullington <tswmcello@me.com>
2017-01-27 01:25:08 -0500

    Update LED_Control.py

--- LED_Control.py
+++ LED_Control.py
@@ -12,47 +12,47 @@ while True:
     
     #Ramps brightness from 0 to 200 and back over 1 second + time to execute
     brightness = 0
-    for n in range(10):
-        brightness += 20 
+    for n in range(100):
+        brightness += 25.5 
         pi1.set_PWM_dutycycle(19, brightness)
-        time.sleep(0.05)
+        time.sleep(0.01)
     
-    for n in range(10):
-        brightness -= 20 
+    for n in range(100):
+        brightness -= 25.5 
         print brightness
         pi1.set_PWM_dutycycle(19, brightness)
-        time.sleep(0.05)
+        time.sleep(0.01)
         
-    for n in range(10):
-        brightness += 20
+    for n in range(100):
+        brightness += 25.5
         pi1.set_PWM_dutycycle(26, brightness)
-        time.sleep(0.05)
+        time.sleep(0.01)
+    
+    for n in range(100):
+        brightness -= 25.5 
+        pi1.set_PWM_dutycycle(26, brightness)
+        time.sleep(0.01)
+    
+    for n in range(100):
+        brightness += 25.5
+        pi1.set_PWM_dutycycle(13, brightness)
+        time.sleep(0.01)
     
     for n in range(10):
         brightness -= 20 
-        pi1.set_PWM_dutycycle(26, brightness)
-        time.sleep(0.05)
-    
-    for n in range(10):
-        brightness += 20
         pi1.set_PWM_dutycycle(13, brightness)
-        time.sleep(0.05)
-    
-    for n in range(10):
-        brightness -= 20 
-        pi1.set_PWM_dutycycle(13, brightness)
-        time.sleep(0.05)
+        time.sleep(0.01)
        
     for n in range(10):
-        brightness += 20 
+        brightness += 25.5 
         pi1.set_PWM_dutycycle(13, brightness)
         pi1.set_PWM_dutycycle(26, brightness)
         pi1.set_PWM_dutycycle(19, brightness)
-        time.sleep(0.05)
+        time.sleep(0.01)
         
-    for n in range(10):
-        brightness -= 20 
+    for n in range(100):
+        brightness -= 25.5 
         pi1.set_PWM_dutycycle(13, brightness)
         pi1.set_PWM_dutycycle(26, brightness)
         pi1.set_PWM_dutycycle(19, brightness)
-        time.sleep(0.05)
+        time.sleep(0.01)
